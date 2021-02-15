816fab1fc553437f82f78e007e54160830727a3f
PavelBeskorovainyi <beskor1993@icloud.com>
2021-02-15 13:52:41 +0200

    test2

--- GithubBrowser 3/GithubBrowser/VC/SearchViewController.swift
+++ GithubBrowser 3/GithubBrowser/VC/SearchViewController.swift
@@ -45,8 +45,8 @@ class SearchViewController: UIViewController {
         self.arrowImage.frame.origin = CGPoint(x: self.tableView.frame.origin.x, y: 150)
         self.arrowImage.contentMode = .redraw
         self.arrowImage.isHidden = false
-         
         self.tableView.isHidden = true
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
